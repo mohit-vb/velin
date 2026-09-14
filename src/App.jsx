771c75588f874d8d313e4a3e6080c70a23f72a90
mohit-vb <mohitvbjadhav@gmail.com>
2026-09-14 22:22:46 +0530

    feat:add context provider in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Navbar, Footer } from "./components/layout";
 import AppRoutes from "./routes/AppRoutes";
+import CartProvider from "./context/CartContext";
 
 export default function App() {
   return (
-    <div className="bg-amber-50 selection:bg-amber-500 selection:text-amber-50">
-      <Navbar />
-      <main>
-        <AppRoutes />
-      </main>
-      <Footer />
-    </div>
+    <CartProvider>
+      <div className="bg-amber-50 selection:bg-amber-500 selection:text-amber-50">
+        <Navbar />
+        <main>
+          <AppRoutes />
+        </main>
+        <Footer />
+      </div>
+    </CartProvider>
   );
 }
